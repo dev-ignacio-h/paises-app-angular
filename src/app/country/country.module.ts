bbeed35681b2bda8ab817d6bf1b1c1e3cce82419
Ignacio <dev.ignacio.h@gmail.com>
2021-02-20 14:52:53 -0300

    create country-table component

--- src/app/country/country.module.ts
+++ src/app/country/country.module.ts
@@ -7,6 +7,7 @@ import { ByCapitalComponent } from './pages/by-capital/by-capital.component';
 import { ByCountryComponent } from './pages/by-country/by-country.component';
 import { ByRegionComponent } from './pages/by-region/by-region.component';
 import { CountryComponent } from './pages/country/country.component';
+import { CountryTableComponent } from './components/country-table/country-table.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { CountryComponent } from './pages/country/country.component';
     ByCountryComponent,
     ByRegionComponent,
     CountryComponent,
+    CountryTableComponent,
   ],
   exports: [
     ByCapitalComponent,
